471bfd91c0058334f18577d56cc8fe9fcd885e0b
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-01-20 14:51:11 -0800

    Fix Package.swift so that the project works on Xcode 12.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/iTwin/mobile-native-ios", .exact("3.0.31")),
+        .package(name: "itwin-mobile-ios", url: "https://github.com/iTwin/mobile-native-ios", .exact("3.0.31")),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "6.15.3"),
         .package(name: "PMKFoundation", url: "https://github.com/PromiseKit/Foundation.git", from: "3.0.0"),
         // The following is a fork of CoreLocation that changes the iOS platform to v9
@@ -36,7 +36,7 @@ let package = Package(
         .target(
             name: "ITwinMobile",
             dependencies: [
-                .product(name: "IModelJsNative", package: "mobile-native-ios"),
+                .product(name: "IModelJsNative", package: "itwin-mobile-ios"),
                 .product(name: "PromiseKit", package: "PromiseKit"),
                 .product(name: "PMKFoundation", package: "PMKFoundation"),
                 .product(name: "PMKCoreLocation", package: "PMKCoreLocation"),

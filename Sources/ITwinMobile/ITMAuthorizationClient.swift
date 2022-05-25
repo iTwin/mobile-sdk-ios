c1e82470ed04d318dc5ae19bbb40f7ff44bf67fb
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-05-25 13:28:13 -0700

    Clean up protocol documenation

--- Sources/ITwinMobile/ITMAuthorizationClient.swift
+++ Sources/ITwinMobile/ITMAuthorizationClient.swift
@@ -7,7 +7,7 @@ import IModelJsNative
 
 /// Protocol that extends `AuthorizationClient` with convenience functionality. A default extension to this protocol implements both of the provided functions.
 public protocol ITMAuthorizationClient: AuthorizationClient {
-    /// The default domain to use in the `error(domain:code:reason:)` function.
+    /// The default domain to use in the ``error(domain:code:reason:)-7jgxz`` function.
     var errorDomain: String { get }
 
     /// Creates and returns an NSError object with the specified settings.
@@ -25,6 +25,11 @@ public protocol ITMAuthorizationClient: AuthorizationClient {
 /// Extension that provides default implementation for the functions in the `ITMAuthorizationClient` protocol.
 public extension ITMAuthorizationClient {
     /// Creates and returns an NSError object with the specified settings. Provides a default value `nil` for `domain` and a default value of `200` for `code`. The `nil` default value for domain causes it to use the value stored in ``errorDomain``.
+    /// - Parameters:
+    ///   - domain: The domain to use for the NSError, default to nil, which uses the value from ``errorDomain``.
+    ///   - code: The code to use for the NSError, defaults 200.
+    ///   - reason: The reason to use for the NSError's NSLocalizedFailureReasonErrorKey userInfo value
+    /// - Returns: An NSError object with the specified values.
     func error(domain: String? = nil, code: Int = 200, reason: String) -> NSError {
         return NSError(domain: domain ?? errorDomain, code: code, userInfo: [NSLocalizedFailureReasonErrorKey: reason])
     }

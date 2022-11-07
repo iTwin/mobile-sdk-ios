7e5a81dac4f6d37a39e7e8467ce5346a8b738840
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-11-07 15:46:45 -0800

    Clean up ITMAlerAction API and make it public

--- Sources/ITwinMobile/ITMAlert.swift
+++ Sources/ITwinMobile/ITMAlert.swift
@@ -8,7 +8,8 @@ import WebKit
 
 // MARK: - Helper classes and extensions for converting the data that comes from WKWebView
 
-enum ITMAlertActionStyle: String, Codable, Equatable {
+/// Type to map between JavaScript and `UIAlertAction.Style`.
+public enum ITMAlertActionStyle: String, Codable, Equatable {
     case `default` = "default"
     case cancel = "cancel"
     case destructive = "destructive"
@@ -27,26 +28,23 @@ extension UIAlertAction.Style {
     }
 }
 
-struct ITMAlertAction: Codable, Equatable {
+/// Swift class that holds data passed from JavaScript for each action in an ``ITMAlert`` and ``ITMActionSheet``.
+public struct ITMAlertAction: Codable, Equatable {
+    /// The name of the alert, passed back to JavaScript when the user selects it.
     let name: String
-    // Note: UIAlertAction's title is in theory optional. However, if the title is missing when used in an action sheet
-    // on an iPad, it will ALWAYS throw an exception. So title here is not optional.
+    /// The title of the alert.
+    /// - Note: UIAlertAction's title is in theory optional. However, if the title is missing when used in an action sheet
+    /// on an iPad, it will ALWAYS throw an exception. So title here is not optional.
     let title: String
+    /// The style of the alert.
     let style: ITMAlertActionStyle
-}
-
-// MARK: - ITMAlert class
-
-/// ``ITMNativeUIComponent`` that presents a `UIAlertController` with a style of `.alert`.
-/// This class is used by the `presentAlert` TypeScript function in @itwin/mobile-core.
-final public class ITMAlert: ITMNativeUIComponent {
-    ///   - itmNativeUI: The ``ITMNativeUI`` used to present the alert.
-    override init(itmNativeUI: ITMNativeUI) {
-        super.init(itmNativeUI: itmNativeUI)
-        queryHandler = itmMessenger.registerQueryHandler("Bentley_ITM_presentAlert", handleQuery)
-    }
-
-    static func extractActions(params: [String: Any], errorPrefix: String) throws -> [ITMAlertAction] {
+    
+    /// Create an array ``ITMAlertAction`` from the given JSON data passed from JavaScript.
+    /// - Parameters:
+    ///   - params: The JSON data for the ``ITMAlertAction`` passed from JavaScript.
+    ///   - errorPrefix: The prefix to use in any error thrown if `params` contains invalid data.
+    /// - Returns: An array of ``ITMAlertAction`` values based on the data in `params`.
+    static func createArray(from params: [String: Any], errorPrefix: String) throws -> [ITMAlertAction] {
         guard let actions = params["actions"] as? [[String: Any]], !actions.isEmpty else {
             throw ITMError(json: ["message": "\(errorPrefix): actions must be present and not empty"])
         }
@@ -56,20 +54,44 @@ final public class ITMAlert: ITMNativeUIComponent {
             throw ITMError(json: ["message": "\(errorPrefix): invalid action"])
         }
     }
+    
+    /// Adds the given array of ``ITMAlertAction`` values to the given `UIAlertController`.
+    /// - Parameters:
+    ///   - actions: The actions to add to the alert controller.
+    ///   - alertController: The `UIAlertController` to add the actions to.
+    ///   - handler: The handler that is called when an action is selected by the `UIAlertController`.
+    static func addActions(_ actions: [ITMAlertAction], to alertController: UIAlertController, handler: ((UIAlertAction, ITMAlertAction) -> Void)? = nil) {
+        for action in actions {
+            alertController.addAction(UIAlertAction(title: action.title, style: UIAlertAction.Style(action.style)) { alertAction in
+                handler?(alertAction, action)
+            })
+        }
+    }
+}
+
+// MARK: - ITMAlert class
+
+/// ``ITMNativeUIComponent`` that presents a `UIAlertController` with a style of `.alert`.
+/// This class is used by the `presentAlert` TypeScript function in @itwin/mobile-core.
+final public class ITMAlert: ITMNativeUIComponent {
+    /// - Parameters:
+    ///   - itmNativeUI: The ``ITMNativeUI`` used to present the alert.
+    override init(itmNativeUI: ITMNativeUI) {
+        super.init(itmNativeUI: itmNativeUI)
+        queryHandler = itmMessenger.registerQueryHandler("Bentley_ITM_presentAlert", handleQuery)
+    }
 
     @MainActor
     private func handleQuery(params: [String: Any]) async throws -> String {
         guard let viewController = viewController else {
             throw ITMError(json: ["message": "ITMAlert: no view controller"])
         }
-        let alertActions = try ITMAlert.extractActions(params: params, errorPrefix: "ITMAlert")
+        let alertActions = try ITMAlertAction.createArray(from: params, errorPrefix: "ITMAlert")
         return await withCheckedContinuation { (continuation: CheckedContinuation<String, Never>) in
             let alert = ITMAlertController(title: params["title"] as? String, message: params["message"] as? String, preferredStyle: .alert)
             alert.showStatusBar = params["showStatusBar"] as? Bool ?? false
-            for action in alertActions {
-                alert.addAction(UIAlertAction(title: action.title, style: UIAlertAction.Style(action.style)) { _ in
-                    continuation.resume(returning: action.name)
-                })
+            ITMAlertAction.addActions(alertActions, to: alert) { _, action in
+                continuation.resume(returning: action.name)
             }
             alert.modalPresentationCapturesStatusBarAppearance = true
             viewController.present(alert, animated: true)

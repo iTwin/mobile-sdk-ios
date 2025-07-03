bb1bc9c1ee2875c0043131da590425c64d0daeea
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2025-07-03 10:58:34 -0600

    Fix git branch in newVersion.py after merge from release/itwin-4.x

--- newVersion.py
+++ newVersion.py
@@ -100,7 +100,7 @@ itwin_version_package = '@itwin/core-common'
 # The package whose dependencies determine the current add-on version.
 native_version_package = '@itwin/core-backend'
 # The branch this script is running in
-git_branch = 'release/itwin-4.x'
+git_branch = 'main'
 # The names of the iOS sample apps
 ios_sample_names = [
     'CameraSample',
